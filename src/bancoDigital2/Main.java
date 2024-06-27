05d893e5370b5ae8af824dd625fb72acbed66b00
Thiago Silva <thiago.egidio.44@gmail.com>
2024-06-27 23:27:03 +0100

    Terceira Atualizacao

--- src/bancoDigital2/Main.java
+++ src/bancoDigital2/Main.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class Main {
 	  public static void main(String[] args) {
-	        String nome = "Clark Kent";
-	        String tipoConta = "Corrente";
+	        String nome = "Peter PArk";
+	        String tipoConta = "Poupanca";
 	        double saldo = 1599.99;
 	        int opcao = 0;
 
